e982b191bc580d70252fead2b37c98b170a7a535
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-04-03 18:33:20 +0800

    本地缓存

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,38 @@
 import { createStore } from 'vuex'
 
-export default createStore({
+/**
+ * 获取本地储存
+ * @param key
+ * @returns {string}
+ */
+function getItem(key) {
+    return window.localStorage.getItem(key)
+}
+
+/**
+ * 设置本地储存与vuex
+ * @param state
+ * @param key
+ * @param value
+ */
+function setItem(state, key, value) {
+    state[key] = value;
+    window.localStorage.setItem(key, value)
+}
+
+const store = createStore({
   state: {
+    isCollapse: getItem('isCollapse')
   },
   mutations: {
+    setIsCollapse(state, value) {
+        setItem(state, 'isCollapse', value)
+    },
   },
   actions: {
   },
   modules: {
   }
 })
+
+export default store
